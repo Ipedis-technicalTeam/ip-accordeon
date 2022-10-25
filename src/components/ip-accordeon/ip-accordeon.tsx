69c607055ad230634340f1dbe28f9caae4efb590
prakash <prakash@ipedis.com>
2022-10-25 16:13:14 +0400

    acc-prakash - implementation accordeon

--- src/components/ip-accordeon/ip-accordeon.tsx
+++ src/components/ip-accordeon/ip-accordeon.tsx
@@ -1,4 +1,5 @@
-import {Component, h, Element, State} from "@stencil/core";
+import {Component, h, Element, Prop, Watch, State} from "@stencil/core";
+import {AccordeonHeadersInterface} from "./interface/accordeon-headers.interface";
 
 @Component({
   tag: 'ip-accordeon',
@@ -9,37 +10,95 @@ export class IpAccordeon {
 
   @Element() el: HTMLElement;
 
-  @State() accButtons;
+  private _accordeonHeaders: AccordeonHeadersInterface[];
+  @Prop() accordeonHeaders: AccordeonHeadersInterface[] | string;
+
+  @Watch('accordeonHeaders')
+  arrayDataWatcher(newValue: AccordeonHeadersInterface[] | string) {
+    if (typeof newValue === 'string') {
+      this._accordeonHeaders = JSON.parse(newValue);
+    } else {
+      this._accordeonHeaders = newValue;
+    }
+  }
+
+  @State() accHeaderButtons;
+  @State() accPanels;
 
   componentWillLoad() {
+    this.arrayDataWatcher(this.accordeonHeaders);
+  }
 
-    const accButtons = this.el.querySelectorAll('.js-acc-button button');
-    const accPanels = this.el.querySelectorAll('.js-acc-panel');
+  componentDidLoad() {
 
-    accButtons.forEach((accButton, index) => {
-      accButton.addEventListener('click', ()=> {
-        if (accPanels[index].classList.contains('js-acc-panel-hide')) {
-          accPanels[index].classList.remove('js-acc-panel-hide');
-        } else {
-          accPanels[index].classList.add('js-acc-panel-hide');
-        }
+    this.accHeaderButtons = this.el.shadowRoot.querySelector('#ip-accordeon').querySelectorAll('button');
+    this.accPanels = this.el.shadowRoot.querySelector('#ip-accordeon').querySelectorAll('.js-panel');
 
-        if (accButton.getAttributeNode('aria-expanded').value === 'true') {
-          accButton.setAttribute('aria-expanded', 'false');
-        } else {
-          accButton.setAttribute('aria-expanded', 'true');
-        }
-      })
-    })
+    // By default set first item to open
+    (this.accHeaderButtons[0] as HTMLElement).setAttribute('aria-expanded', 'true');
+    (this.accPanels[0] as HTMLElement).classList.remove('js-panel-hide');
 
+    // For accessibility - set an id on the slotted elements
+    const slottedElems = this.el.querySelectorAll('[slot]');
+    slottedElems.forEach((slotElem, index) => {
+      slotElem.setAttribute('id', 'sect-' + (index+1))
+    });
 
+  }
+
+  onSelectPanel(index: number) {
+
+    const selectedButton = this.accHeaderButtons[index - 1];
+    const selectedPanel = this.accPanels[index - 1];
+
+    if (selectedPanel.classList.contains('js-panel-hide')) {
+      selectedPanel.classList.remove('js-panel-hide');
+    } else {
+      selectedPanel.classList.add('js-panel-hide');
+    }
+
+    if (selectedButton.getAttributeNode('aria-expanded').value === 'true') {
+      selectedButton.setAttribute('aria-expanded', 'false');
+    } else {
+      selectedButton.setAttribute('aria-expanded', 'true');
+    }
   }
 
   render() {
-    return <div class='acc-contents'>
-      <slot name='accordeon-1'></slot>
-      <slot name='accordeon-2'></slot>
-      <slot name='accordeon-3'></slot>
-    </div>
+
+    const accPanels = (
+      <div>
+        {this._accordeonHeaders.map((tabHeader, index) => (
+            [
+              <h3 class="js-acc-button">
+
+                <button
+                  onClick={this.onSelectPanel.bind(this,  (index + 1))}
+                  aria-expanded="false"
+                  aria-controls={`sect-${index + 1}`}
+                  id={`accordeon-${index + 1}`}>
+                  <span class="accordion-title">
+                    {tabHeader.title}
+                  </span>
+                  <span>^</span>
+                </button>
+
+              </h3>,
+              <div id={`sect-${index + 1}`} role="region" aria-labelledby={`accordeon-${index + 1}`} class='js-panel js-panel-hide'>
+                <slot name={'accordeon-' + (index + 1)}></slot>
+              </div>
+
+            ]
+          ))
+        }
+      </div>
+    )
+
+    return [
+      <div class='ip-accordeon' id='ip-accordeon'>
+        { accPanels }
+      </div>
+    ]
   }
+
 }
